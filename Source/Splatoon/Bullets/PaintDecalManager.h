ccc14ce9c4aee1ac5d0f957bf9d07eb86338a15a
haeuny123 <haeuny123@gmail.com>
2025-02-20 18:19:14 +0900

    [fix] 탄환 크래시 해결 #22

---
+++ Source/Splatoon/Bullets/PaintDecalManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "PaintDecalManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPLATOON_API UPaintDecalManager : public UObject
+{
+	GENERATED_BODY()
+	
+};
