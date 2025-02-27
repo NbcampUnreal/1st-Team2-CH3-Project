0fb5297bf841b07dcb0297547b023586be79ab77
dohun <ehgns1524@naver.com>
2025-02-27 12:28:37 +0900

    [feat] Ranged AI Developing
    Ranged AI Developing

---
+++ Source/Splatoon/Bullets/EnemyPaintDecal.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Bullets/PaintDecal.h"
+#include "EnemyPaintDecal.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPLATOON_API AEnemyPaintDecal : public APaintDecal
+{
+	GENERATED_BODY()
+	
+};
