e46fe9a3f109c2ccddff45362ff2f10f5bc58b83
burperring <khw5231@gmail.com>
2025-03-04 16:13:20 +0900

    [feat] GameInstance

---
+++ Source/Splatoon/Games/SplatoonGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SplatoonGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPLATOON_API USplatoonGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
